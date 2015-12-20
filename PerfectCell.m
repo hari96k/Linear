1b98eb95e104c234ad8736d4b0e825b3cdb354e1
Hari Krishna Kosuru <hari96k@utexas.edu>
2015-12-19 21:24:24 -0600

    Added square and rectangle detection

--- PerfectCell.m
+++ PerfectCell.m
@@ -14,15 +14,23 @@ commandwindow;
 
 img_many = imread('images/test/canny1.jpg');
 img_bad = imread('images/test/bad.png');
-img_reallybad = imread('images/test/copy.png');
-img_croppedt = imread('images/crop2.jpg');
-
+% img_test = imread('images/test/test2.jpg');
+% img_reallybad = imread('images/test/copy.png');
+% img_croppedt = imread('images/crop2.jpg');
+img_square = imread('images/test/square.jpg');
+img_square2 = imread('images/test/square2.jpg');
+img_square3 = imread('images/test/square3.jpg');
+img_rectangle = imread('images/test/rectangle.jpg');
+img_rectangle2 = imread('images/test/rectangle2.jpg');
+img_rectangle3 = imread('images/test/rectangle3.jpg');
+img_barelyRectangle = imread('images/test/barelyRectangle.jpg');
 
 % Change the img and c_image assignment to debug with another image
 
 
-img = img_many;
+img = img_rectangle3;
 
+%Small Triangle (img_bad)
 % RGB2 = imadjust(img,[.1 .1 .1; .9 .9 .9]);
 % gray = rgb2gray(RGB2);
 % ed = edge(gray, 'canny', .2);
@@ -30,19 +38,27 @@ img = img_many;
 
 
 %Circle
-c_image = imcrop (im2bw(img, 0.4), [650 400 950 650]);
+% c_image = imcrop (im2bw(img, 0.4), [650 400 950 650]);
 
 %Triangle
 % c_image = imcrop (im2bw(img, 0.4), [200 300 400 400]);
 
+%Square or Rectangle
+ RGB2 = imadjust(img,[.1 .1 .1; .9 .9 .9]);
+ gray = rgb2gray(RGB2);
+ c_image = edge(gray, 'canny', .2);
+ 
+
 repeat = 1;
 noiseLVL = 0;
+% rotated = 0;
 
 while  repeat && noiseLVL < 6
 
 c_image = bwareaopen(c_image, 500*noiseLVL);
 
-figure;hold on;imshow(c_image);axis on;
+figure;hold on;
+imshow(c_image);axis on;
 
 % Creates the row/col arrays where the image is white (1)
 [yArray, xArray] =   find (c_image==1);
@@ -55,6 +71,7 @@ cornersArray = [];
 xyIndex = 1;
 prevEnd = 1;
 i = 1;
+j = 1;
 
 % Global error handle, catches all exceptions
  try
@@ -81,16 +98,25 @@ i = 1;
 
                 xyIndex = n_xyIndex;
                 i= i+1;
+                j= j+1;
             end
             % Determines if the traversal is long enough to be a side
-            if (i-prevEnd > .1*i + 5)
+            if (abs(j-prevEnd) > .1*j + 5)
                 line ([xArray(index(prevEnd)), xArray(index(i-2))], [yArray(index(prevEnd)), yArray(index(i-2))],'Color','b','LineWidth',4);
                 cornersArray = vertcat(cornersArray,[index(prevEnd), index(i-1)]);
                 prevEnd = i;
             end
         end
-        start = start - 1;
+        
+        %Reversing traversal direction after first iteration
+        if (start == 1)
+            start = 0;
+        else
+            start = 1;
+        end
+        
         prevEnd = 1;
+        j = 1;
         [xyIndex,~] = mimicShadow( yArray, xArray, yArray(index(2)), xArray(index(2)));
     end
 
@@ -102,13 +128,22 @@ i = 1;
         repeat = 0;
     end
 
-    noiseLVL = noiseLVL + 1;
     title(shape);
 
 catch
     title('Something went wrong')
 end
 
+    noiseLVL = noiseLVL + 1;
+    
+%     if (rotated == 0)
+%         img = imrotate(img, 30, 'loose');
+%         RGB2 = imadjust(img,[.1 .1 .1; .9 .9 .9]);
+%         gray = rgb2gray(RGB2);
+%         c_image = edge(gray, 'canny', .2);
+%         figure;hold on;
+%         imshow(c_image);axis on;
+%         repeat = 1;
+%         rotated = 1;
+%     end
 end
-
-
--- discriminate.m
+++ discriminate.m
@@ -2,6 +2,8 @@ function [ string ] = discriminate( corners, x, y, index )
 
 [realsize,~] = size(corners);
 
+string = 'Unknown' ;
+
 if realsize == 2 || realsize == 1 ;
     string = 'Line';
     return
@@ -32,6 +34,7 @@ if realsize > 4
     return
 end
 
+%Anything beyond this point must be a square, rectangle, circle, or unknown
 
     for b = 1:4
         xmid = ( ( x(corners(b,2)) ) + x(corners(b,1)) ) / 2;
@@ -40,7 +43,7 @@ end
         condition = condition + notAlone( xmid , ymid , xEdges, yEdges );    
     end
 
-if (condition < 2)
+if (condition < 2)      %Catches circles
     
     xrad = (x(corners(4,2)) - x(corners(1,1)))/2 ;
     if ( y(corners(2,1) < y(corners(4,1))) )
@@ -62,6 +65,22 @@ if (condition < 2)
 
     string = 'Circle';
     return
+end
+
+length = zeros([4 1]);
+
+condition = 0;
+
+for c = 1:4
+    deltax = x(corners(c,2)) - x(corners(c,1)) ;
+    deltay = y(corners(c,2)) - y(corners(c,1)) ;
+    length(c) = sqrt(  deltax^2  + deltay^2 ) ;
+end
+
+if (( max(length) - min(length)) < .25* mean(length) )
+    string = 'Square';
+else
+    string = 'Rectangle';
 end
 
 
