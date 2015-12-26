d00d8a74f67358caa95310cde10ed266f9cb6b02
Hari Krishna Kosuru <hari96k@utexas.edu>
2015-12-25 23:23:54 -0600

    Trapezoid detection significantly increased. Cross detection added

--- damnStraight.m
+++ damnStraight.m
@@ -1,46 +1,4 @@
-function [ startxarray, startyarray, endxarray, endyarray ] = damnStraight( xarray, yarray, slopearray )
-
-i = 2;
-
-startxarray(1) = xarray(2);
-startyarray(1) = yarray(2);
-[~,breakNumber] = size(slopearray); 
-while ( abs ( slopearray(2) - slopearray(i+1) ) < 1.5 )
-    i = i + 1;
-    if (i >= breakNumber )
-    break;
-    end    
-end
-
-endxarray(1) = xarray(i);
-endyarray(1) = yarray(i);
-
-if i == breakNumber
-   return
-end
-
-
-
-
-m = i;
-
-startxarray(2) = endxarray(1);
-startyarray(2) = endyarray(1);
-
-
-% while ( abs ( slopearray(m) - slopearray(i+1) ) < 1.5 )
-%     i = i + 1;
-% end
-
-endxarray(2) = xarray(19);
-endyarray(2) = yarray(19);
-
-%  for n = m:19
-%     modearray(n) = slopearray(n);
-%  end
-% 
-% slopearray(1) = mode(modearray);
-
+function [ numPerp, numPar ] = damnStraight( corners )
 
 
 end
